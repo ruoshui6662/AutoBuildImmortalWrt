cf0261c65c7e18eae63cb34e00be7868e53a1bc7
ruoshui6662 <110121567+ruoshui6662@users.noreply.github.com>
2026-01-23 13:16:24 +0800

    Add Tailscale language package to build script

--- n1/build.sh
+++ n1/build.sh
@@ -31,6 +31,8 @@ PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+# tailscale
+PACKAGES="$PACKAGES luci-i18n-tailscale-zh-cn"
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
